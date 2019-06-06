867b760eddc8ea79417f02090a614411b7732081
kyraNET <kyradiscord@gmail.com>
2019-06-06 13:47:42 +0200

    misc: Fixed build

--- src/index.ts
+++ src/index.ts
@@ -10,5 +10,13 @@ const wasmModule = instantiateBuffer<{
 export function levenshtein(a: string, b: string): number {
 	if (a === b) return 0;
 	if (a.length > b.length) [a, b] = [b, a];
-	return wasmModule.levenshtein(wasmModule.newString(a), wasmModule.newString(b));
+
+	const ptrB = wasmModule.__retain(wasmModule.__allocString(b));
+	const ptrA = wasmModule.__retain(wasmModule.__allocString(a));
+	try {
+		return wasmModule.levenshtein(ptrA, ptrB);
+	} finally {
+		wasmModule.__release(ptrA);
+		wasmModule.__release(ptrB);
+	}
 }
