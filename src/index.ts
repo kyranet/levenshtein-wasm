f4cd19de72d4bcba24c1945a875a34f063247939
kyraNET <kyradiscord@gmail.com>
2019-06-07 09:28:12 +0200

    revert: Allocated memory should be retained and released

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,12 @@ export function levenshtein(a: string, b: string): number {
 	if (a === b) return 0;
 	if (a.length > b.length) [a, b] = [b, a];
 
-	const ptrB = wasmModule.__allocString(b);
-	const ptrA = wasmModule.__allocString(a);
-	return wasmModule.levenshtein(ptrA, ptrB);
+	const ptrB = wasmModule.__retain(wasmModule.__allocString(b));
+	const ptrA = wasmModule.__retain(wasmModule.__allocString(a));
+	try {
+		return wasmModule.levenshtein(ptrA, ptrB);
+	} finally {
+		wasmModule.__release(ptrA);
+		wasmModule.__release(ptrB);
+	}
 }
