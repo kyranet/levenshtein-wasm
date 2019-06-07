7c86572b182aafc1777fd70d76940577b3452f61
kyraNET <kyradiscord@gmail.com>
2019-06-07 09:25:22 +0200

    perf: Change algorithm to js-levenshtein's

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,63 +1,95 @@
-// Copyright (c) Sindre Sorhus <sindresorhus@gmail.com>. All rights reserved. MIT license.
-// This is a derivative work of https://github.com/sindresorhus/leven in AssemblyScript.
+// Copyright (c) Gustaf Andersson. All rights reserved. MIT license.
+// This is a derivative work of https://github.com/gustf/js-levenshtein in AssemblyScript.
 
-/* global memory, usize */
+/* global usize */
 /* eslint-disable prefer-const, @typescript-eslint/restrict-plus-operands */
 
 // @ts-ignore
-export { memory };
+@inline function min(d0: i32, d1: i32, d2: i32, bx: i32, ay: i32): i32 {
+	return d0 < d1 || d2 < d1
+		? d0 > d2
+			? d2 + 1
+			: d0 + 1
+		: bx === ay
+			? d1
+			: d1 + 1;
+}
 
-export function levenshtein(left: string, right: string): usize {
-	let leftLength: usize = left.length;
-	let rightLength: usize = right.length;
+export function levenshtein(a: string, b: string): usize {
+	let la: usize = a.length;
+	let lb: usize = b.length;
 
 	// Perform suffix suffix trimming
-	while (leftLength > 0 && (left.charCodeAt(~-leftLength) === right.charCodeAt(~-rightLength))) {
-		--leftLength;
-		--rightLength;
+	while (la > 0 && (a.charCodeAt(la - 1) === b.charCodeAt(lb - 1))) {
+		--la;
+		--lb;
 	}
 
 	// Perform prefix trimming
-	let start: usize = 0;
-	while (start < leftLength && (left.charCodeAt(start) === right.charCodeAt(start))) {
-		++start;
+	let offset: usize = 0;
+	while (offset < la && (a.charCodeAt(offset) === b.charCodeAt(offset))) {
+		++offset;
 	}
 
-	leftLength -= start;
-	if (leftLength === 0) {
-		return rightLength;
+	la -= offset;
+	lb -= offset;
+	if (la === 0 || lb < 3) {
+		return lb;
 	}
-	rightLength -= start;
 
-	let bCharCode: u16;
-	let result: usize = usize.MAX_VALUE;
-	let temp: u16;
-	let temp2: u16;
-	let i: u16 = 0;
-	let j: u16 = 0;
+	let x: usize = 0;
+	let y: usize = 0;
+	let d0: i32;
+	let d1: i32;
+	let d2: i32;
+	let d3: i32;
+	let dd: usize = usize.MAX_VALUE;
+	let dy: i32;
+	let ay: i32;
+	let bx0: i32;
+	let bx1: i32;
+	let bx2: i32;
+	let bx3: i32;
 
-	let view: Uint16Array = new Uint16Array(leftLength);
-	while (i < leftLength) view[i] = ++i;
+	let view: Int32Array = new Int32Array(la * 2);
+	for (let i: i32 = 0; i < view.length; ++y) {
+		view[i++] = y + 1;
+		view[i++] = a.charCodeAt(offset + y);
+	}
 
-	while (j < rightLength) {
-		bCharCode = right.charCodeAt(start + j) as u16;
-		temp = j++;
-		result = j;
-		i = 0;
-
-		for (i = 0; i < leftLength; ++i) {
-			temp2 = bCharCode === left.charCodeAt(start + i) ? temp : temp + 1;
-			temp = view[i];
-			result = temp > result
-				? temp2 > result
-					? result + 1
-					: temp2
-				: temp2 > temp
-					? temp + 1
-					: temp2;
-			view[i] = result;
+	let len: usize = view.length - 1;
+	while (x < lb - 3) {
+		bx0 = b.charCodeAt(offset + (d0 = x));
+		bx1 = b.charCodeAt(offset + (d1 = x + 1));
+		bx2 = b.charCodeAt(offset + (d2 = x + 2));
+		bx3 = b.charCodeAt(offset + (d3 = x + 3));
+		x += 4;
+		dd = x;
+		for (y = 0; y < len; y += 2) {
+			dy = view[y];
+			ay = view[y + 1];
+			d0 = min(dy, d0, d1, bx0, ay);
+			d1 = min(d0, d1, d2, bx1, ay);
+			d2 = min(d1, d2, d3, bx2, ay);
+			dd = min(d2, d3, dd, bx3, ay);
+			view[y] = dd;
+			d3 = d2;
+			d2 = d1;
+			d1 = d0;
+			d0 = dy;
 		}
 	}
 
-	return result;
+	while (x < lb) {
+		bx0 = b.charCodeAt(offset + (d0 = x));
+		dd = ++x;
+		for (y = 0; y < len; y += 2) {
+			dy = view[y];
+			dd = min(dy, d0, dd, bx0, view[y + 1]);
+			view[y] = dd;
+			d0 = dy;
+		}
+	}
+
+	return dd;
 }
